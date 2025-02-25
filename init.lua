3dd33f8e006d646102f2ab6e4541d98fd56851b8
Rômulo Maciel <romulo.maciel@sap.com>
2025-02-24 22:51:15 -0300

    chore: update colorscheme to tokyonight

--- init.lua
+++ init.lua
@@ -409,11 +409,11 @@ require('lazy').setup({
   },
 
   {
-    'lalitmee/cobalt2.nvim',
+    'folke/tokyonight.nvim',
     event = { 'ColorSchemePre' }, -- if you want to lazy load
     dependencies = { 'tjdevries/colorbuddy.nvim', tag = 'v1.0.0' },
     init = function()
-      require('colorbuddy').colorscheme 'cobalt2'
+      require('colorbuddy').colorscheme 'tokyonight-night'
     end,
   },
 
